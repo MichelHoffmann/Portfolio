3e2b42b3b6f584a83988859557fdf028ac561057
MichelHoffmann <mic.hoff.jr@gmail.com>
2025-08-22 11:40:44 -0500

    chore: ajuste de regras do ESLint

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,21 @@ export function Home() {
   return (
     <>
       <Header />
+      <header className="flex justify-between px-9">
+        <h1 className="text-[var(--purple-dark)]">MH</h1>
+        <nav>
+          <ul className="flex">
+            <li>
+              <a href="#" className="text-amber-200">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#">ContactMe</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <AboutMe />
     </>
   );
